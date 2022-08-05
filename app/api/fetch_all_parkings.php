266f22082ecda386c654dfd281d38eaa4eb0f84b
Maxime Princelle <maxime@princelle.org>
2022-08-05 12:30:07 +0200

    ⚡️ caching fetch_all_parkings

--- app/api/fetch_all_parkings.php
+++ app/api/fetch_all_parkings.php
@@ -8,77 +8,47 @@
 # Fetch parkings in Strasbourg through the API
 # https://data.strasbourg.eu/api/records/1.0/search/?dataset=parkings&q=&lang=fr&timezone=Europe%2FParis
 function fetch_all_parkings() {
-    $parkings = [];
+    // Initiate SQLLite Database connection
+    $db = new PDO('sqlite:'.dirname(__FILE__).'/../db/parkings.db');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $url = 'https://data.strasbourg.eu/api/records/1.0/search/?dataset=parkings&q=&lang=fr&timezone=Europe%2FParis';
+    // Get all parkings from the database
+    $stmt = $db->prepare('SELECT * FROM parkings');
+    $stmt->execute();
 
-    $url .= '&rows=' . 200;
+    // Fetch all parkings
+    $parkings = $stmt->fetchAll();
 
-    $json = file_get_contents($url);
-    $data = json_decode($json, true);
-
-    # For Each $data->records
-    foreach ($data['records'] as $record) {
-        # Fetch Parking details : 
-        #https://data.strasbourg.eu/api/records/1.0/search/?dataset=occupation-parkings-temps-reel&q=&lang=fr&timezone=Europe%2FParis&refine.idsurfs='.$record['fields']['idsurfs']
-
-        $parking_details_url = 'https://data.strasbourg.eu/api/records/1.0/search/?dataset=occupation-parkings-temps-reel&q=&lang=fr&timezone=Europe%2FParis&refine.idsurfs='.$record['fields']['idsurfs'];
-
-        $parking_details_json = file_get_contents($parking_details_url);
-        $parking_details_data = json_decode($parking_details_json, true);
-
-        $parking = [
-            "id" => $record['fields']['idsurfs'],
-            "name" => $record['fields']['name'],
-            "address" => $record['fields']['address'],
-            "position" => [
-                "lat" => $record['fields']['position'][0],
-                "lng" => $record['fields']['position'][1]
-            ],
-            "description" => isset($record['fields']['description']) ? $record['fields']['description'] : null,
-            "url" => $record['fields']['friendlyurl'],
+    // Parse all parkings
+    $parkings_parsed = [];
+    foreach ($parkings as $parking) {
+        $temp_parking = [
+            "id" => $parking['id'],
+            "name" => $parking['name'],
+            "address" => $parking['address'],
+            "position" => json_decode($parking['position'], true),
+            "description" => json_decode($parking['description']),
+            "url" => $parking['url'],
         ];
 
-        // Format description
-        if ($parking['description'] != null) {
-            $parking['description'] = str_replace("     ", "\n\n", $parking['description']);
-            $parking['description'] = str_replace("    ", "\n\n", $parking['description']);
-            $parking['description'] = str_replace("   ", "\n\n", $parking['description']);
-            $parking['description'] = str_replace("  ", "\n\n", $parking['description']);
-            $parking['description'] = str_replace(" - ", "\n- ", $parking['description']);
+        // Add parking details (occupation, etat, updated_at) if they exist
+        if ($parking['occupation'] != "null") {
+            $temp_parking['occupation'] = json_decode($parking['occupation'], true);
         }
 
-        // Add parking details if they exist
-        if (isset($parking_details_data['records'][0])) {
-            $parking_details = $parking_details_data['records'][0]['fields'];
-            $details = [
-                "occupation" => [
-                    "available" => $parking_details['libre'],
-                    "total" => $parking_details['total'],
-                    "occupied" => $parking_details['total'] - $parking_details['libre'],
-                    "percentage" => round(($parking_details['total'] - $parking_details['libre']) / $parking_details['total'] * 100, 2)
-                ],
-                "etat" => $parking_details['etat'],
-                "updated_at" => $parking_details_data['records'][0]['record_timestamp']
-            ];
-
-            if ($details["etat"] == 0) {
-                // Delete occupation from details
-                unset($details["occupation"]);
-
-                // If infousager is OUVERT, set etat to 1 (OPEN)
-                if ($parking_details["infousager"] == "OUVERT") {
-                    $details["etat"] = 1;
-                }
-            }
-
-            $parking = array_merge($parking, $details);
+        if ($parking['etat'] != "null") {
+            $temp_parking['etat'] = $parking['etat'];
         }
 
-        array_push($parkings, $parking);
+        if ($parking['updated_at'] != "null") {
+            $temp_parking['updated_at'] = $parking['updated_at'];
+        }
+
+        // Add parking to the array
+        $parkings_parsed[] = $temp_parking;
     }
 
-    return $parkings;
+    return $parkings_parsed;
 }
 
 # Fetch parkings in Strasbourg through the API
